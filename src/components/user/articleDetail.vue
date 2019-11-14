881d9dae1b897c7b9f5de1c12c9f69b2cd9042e2
mmxuehan <942741468@qq.com>
2019-11-14 15:45:43 +0800

    tijiao

--- src/components/user/articleDetail.vue
+++ src/components/user/articleDetail.vue
@@ -22,19 +22,84 @@
       </div>
     </div>
     <!-- 评论模块 -->
+    <h3 class="text-title">发表评论</h3>
+    <el-input
+      type="textarea"
+      :rows="4"
+      placeholder="请输入内容"
+      v-model="commentText"
+      resize="none"
+      class="textacer"
+    ></el-input>
+    <el-button type="info" @click="textsubmit" class="submitArt" plain>发表</el-button>
+    <!-- 查看评论模块 -->
+    <h3 class="counttitle">{{tSlip}}条评论</h3>
+    <div class="commentItem" v-for="(item, index) in commentItem" :key="index">
+      <!-- 评论的内容 -->
+      <p>{{item.commentContent}}</p>
+      <div class="art-icon">
+        <span class="creat-time">
+          <i class="iconfont icon-dingdanxiangqing-chuangjianshijian" title="发表时间"></i>
+          {{item.createTime}}
+        </span>
+        <span class="update-time">
+          <i class="iconfont icon-gengxinshijian" title="更新时间"></i>
+          {{item.updateTime}}
+        </span>
+        <span class="reply">0 回复</span>
+        <div class="underline"></div>
+      </div>
+    </div>
+
+    <div class="commentItem">
+      <!-- 评论的内容 -->
+
+      <p ref="maxheight">
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论
+        的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论
+        的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论
+        的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+        我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论
+        的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈我是评论的内容哈哈哈哈哈
+      </p>
+      <div class="art-icon">
+        <span class="creat-time">
+          <i class="iconfont icon-dingdanxiangqing-chuangjianshijian" title="发表时间"></i>
+          2019-11-2
+        </span>
+        <span class="update-time">
+          <i class="iconfont icon-gengxinshijian" title="更新时间"></i>
+          2020-11-3
+        </span>
+        <span class="reply">0 回复</span>
+        <div class="underline"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import qs from "qs";
 import { ajax } from "../../network/request";
 export default {
-  name:'detailData',
+  name: "detailData",
   data() {
     return {
       // 初始化detailData
       detailData: null,
       // 初始化定时器
-      timerId: ""
+      timerId: "",
+      //初始化文章的评论内容
+      commentText: "",
+
+      //初始化每条评论的数据，保存服务器请求回来的数据
+      commentItem: [],
+
     };
   },
   computed: {
@@ -45,37 +110,97 @@ export default {
     //初始化articleId
     articleId() {
       return this.detailData.article_id;
+    },
+    userName() {
+      return this.$store.state.userName;
+    },
+    token() {
+      return this.$store.state.token;
+    },
+    //总条数
+    tSlip(){
+      return this.commentItem.length;
+    }
+  },
+  methods: {
+    textsubmit() {
+      //发表评论的按钮，向服务器发送请求
+      let data = {
+        commentFarther: 0,
+        commintContent: this.commentText,
+        userName: this.userName,
+        conmentArtcleId: this.articleId
+      };
+      ajax({
+        url: "/upload/insertComment",
+        method: "post",
+        data: qs.stringify(data),
+        //热度加一需不需要发token
+        headers: { accessToken: this.token }
+      }).then(res => {
+        if (res.status == 0) {
+          //发表成功
+          this.$message("评论成功");
+          this.commentText = "";
+        }
+      });
+    },
+    // 点击回复按钮，回复对应的评论
+    reply() {
+      //设置最大宽度增大
+      this.$refs.maxheight.style.maxHeight = "300px";
+      //显示
+      alert("sssss");
     }
   },
   created() {
     this.detailData = this.$route.query;
-    console.log("detaildata为" + this.$route.query);
-    
+    // console.log("detaildata为" + this.$route.query);
+  },
+  mounted() {
+    //发送请求请求回来
+    let data = {
+      articleId: this.articleId,
+      userName: this.userName
+    };
+    ajax({
+      url: "/upload/selectComment",
+      method: "post",
+      data: qs.stringify(data),
+      //发token
+      headers: { accessToken: this.token }
+    }).then(res => {
+      if (res.status == 0) {
+        //发表成功
+        this.commentItem = res.data;
+        // 渲染到页面上
+      }
+    });
   },
   beforeRouteEnter(to, from, next) {
     next(vm => {
       // 通过 `vm` 访问组件实例
       //开启一个定时器，如果超过了1分钟就发送请求把articleHeat加一传过去
-    vm.timerId = setTimeout(() => {
-      let data = {
-        articleId: vm.articleId,
-        articleHeat: vm.articleHeat++
-      };
-      ajax({
-        url: "/upload/updateArticleHeat",
-        method: "post",
-        data: qs.stringify(data),
-        //热度加一需不需要发token
-        headers: { token: vm.token }
-      }).then(res => {
-        if (res.status == 0) {
-          //热度增加成功
-          vm.$message("文章热度增加成功");
-          //销毁定时器
-          clearTimeout(vm.timerId);
-        }
-      });
-    }, 10000);
+      vm.timerId = setTimeout(() => {
+        let data = {
+          articleId: vm.articleId,
+          articleHeat: vm.articleHeat++
+        };
+        ajax({
+          url: "/upload/updateArticleHeat",
+          method: "post",
+          data: qs.stringify(data)
+          //热度加一需不需要发token
+          //headers: { token: vm.token }
+        }).then(res => {
+          if (res.status == 0) {
+            //热度增加成功
+            vm.$message("文章热度增加成功");
+            //销毁定时器
+            clearTimeout(vm.timerId);
+          }
+        });
+      }, 10000);
     });
   }
 };
@@ -93,6 +218,16 @@ export default {
   color: #404040;
   margin-top: 20px;
   margin-bottom: 20px;
+}
+.counttitle {
+  margin-top: 100px;
+}
+.commentItem p {
+  margin-bottom: 30px;
+  max-width: 900px;
+  max-height: 85px;
+  overflow: hidden;
+  text-indent: 1em;
 }
 
 .art-title {
@@ -112,5 +247,32 @@ export default {
 }
 .heat-icon i {
   color: #ea6f5a;
+}
+.text-title {
+  margin-top: 30px;
+  color: #0090ff;
+}
+.textacer {
+  width: 90%;
+  margin: 20px auto;
+}
+.underline {
+  height: 1px;
+  background-color: #f0f0f0;
+  width: 900px;
+  box-shadow: 0px -1px 1px 0px #fbfbfb;
+}
+.submitArt {
+  position: relative;
+  top: 30px;
+  right: 100px;
+}
+.reply {
+  color: #2f93ff;
+  margin-left: 20px;
+  cursor: pointer;
+}
+.commentItem {
+  margin-top: 40px;
 }
 </style>
